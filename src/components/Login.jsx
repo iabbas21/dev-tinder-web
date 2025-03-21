bb67a2997f66125d3f66c6acb3f124074442938c
irfan.abbas@soprasteria.com <irfan.abbas@soprasteria.com>
2025-03-21 22:01:29 +0530

    created login page & configured redux in application

---
+++ src/components/Login.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
+import { useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../utils/constants';
+
+const Login = () => {
+  const [emailId, setEmailId] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(BASE_URL + '/login', {
+        emailId,
+        password
+      }, {
+        withCredentials: true
+      })
+      console.log(res);
+      dispatch(addUser(res.data));
+      navigate('/');
+    } catch(error) {
+        console.log(error.message);
+    }
+  }
+
+  return (
+    <div className='flex items-center justify-center my-20'>
+        <div className="card bg-base-200 w-96 shadow-sm">   
+            <div className="card-body">
+                <h2 className="card-title text-center">Login</h2>
+                <div>
+                    <div className='my-3'>
+                      <input 
+                        type="text" 
+                        placeholder="Email Id" 
+                        className="input"
+                        value={emailId}
+                        onChange={(e) => setEmailId(e.target.value)}
+                      />
+                    </div>
+                    <div className='my-3'>
+                      <input 
+                        type="password" 
+                        placeholder="Password" 
+                        className="input"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                    </div>
+                    <div className='text-center my-3'>
+                        <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
