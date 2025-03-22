29625e4af51f2a6ff1c9802ef55c8606959b5037
irfan.abbas@soprasteria.com <irfan.abbas@soprasteria.com>
2025-03-22 13:08:34 +0530

    handle fetch profile if user refreshes page after login

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NavBar from './NavBar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import Footer from './Footer'
+import axios from 'axios'
+import { BASE_URL } from '../utils/constants'
+import { useDispatch, useSelector } from 'react-redux'
+import { addUser } from '../utils/userSlice'
 
 const Body = () => {
+  const user = useSelector(state => state.user)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(BASE_URL + '/profile/view', { withCredentials: true })
+      dispatch(addUser(res.data))
+    } catch (error) {
+      console.error(error)
+      if(error.status === 401) navigate('/login')
+    }
+  }
+
+  useEffect(() => {
+    if(!user) fetchUser()
+  }, [])
+
   return (
     <div>
       <NavBar />
