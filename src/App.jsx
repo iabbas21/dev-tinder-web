9f86133879dd5097ff3fd79e257fd41758d86a81
irfan.abbas@soprasteria.com <irfan.abbas@soprasteria.com>
2025-03-23 19:48:51 +0530

    connections API

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Body from './components/Body'
 import Login from './components/Login'
 import Profile from './components/Profile'
 import Feed from './components/Feed'
+import Connections from './components/Connections'
 import appStore from './utils/appStore'
 import './App.css'
 
@@ -16,6 +17,8 @@ function App() {
             <Route path='/' element={<Feed />} />
             <Route path='/login' element={<Login />} />
             <Route path='/profile' element={<Profile />} />
+            <Route path='/connections' element={<Connections />} />
+            <Route path='/requests' element={<Profile />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/components/Connections.jsx
@@ -0,0 +1,51 @@
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { BASE_URL } from '../utils/constants'
+import { useSelector, useDispatch } from 'react-redux'
+import { addConnection } from '../utils/connectionSlice'
+
+const Connections = () => {
+  const connections = useSelector(state => state.connection)
+  const dispatch = useDispatch()
+
+  const fetchConnections = async () => {
+    try {
+      const res = await axios.get(BASE_URL + '/user/connections', { withCredentials: true })
+      dispatch(addConnection(res.data))
+    } catch (error) {
+        console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    if(!connections) fetchConnections()
+  }, [])
+
+  return (
+    <div className='flex justify-center my-10'>
+      {
+        connections && connections.length > 0 ? (
+          <div>
+            <h1 className='text-bold text-3xl text-center'>Connections</h1>
+            {connections.map(connection => (
+                <div className='flex items-center gap-3 bg-base-300 p-3 rounded-lg w-200 m-3'>
+                  <div>
+                    <img className='w-15 h-15 rounded-full' alt='photo' src={connection.photoUrl}/>
+                  </div>
+                  <div>
+                    <h2 className='text-bold text-xl'>{connection.firstName + " " + connection.lastName}</h2>
+                    {connection.age && connection.gender && <p>{connection.age + ", " + connection.gender}</p>}
+                    <p>{connection.about}</p>
+                  </div>
+                </div>
+            ))}
+          </div>
+        ) : (
+          <h1>No Connections</h1>
+        )
+      }
+    </div>
+  )
+}
+
+export default Connections
